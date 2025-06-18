bf5ed33740766d7496e6bab63147c74b2846c073
Deepak Roy <deepakroy11@gmail.com>
2025-06-19 00:31:23 +0530

    Fix Prisma generator issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -22,3 +22,8 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
+
+// export { auth as middleware } from "@/auth";
+// export const config = {
+//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+// };
